b5716e66f8d0c34f9abcecb063208bfb5635bffa
Vikas Kumar <vikas.digllia@gmail.com>
2021-06-13 19:17:42 +0530

    Header and about page

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+      <>
+        <img src="/logo.png" width="400" height="93" />
+      </>
+  )
+}
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import Layout from '../components/Layout';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
+

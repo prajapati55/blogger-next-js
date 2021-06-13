689ec1020862894d4cc889ae09ee4d2ff5d677c8
Vikas Kumar <vikas.digllia@gmail.com>
2021-06-13 18:20:19 +0530

    scss

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
+	/* bydefault config  option Read For More Optioshere https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
+	cssModules: true})
+module.exports = {
+    /* Add Your Scss File Folder Path Here */
+	sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+}
